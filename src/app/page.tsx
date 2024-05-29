29e288854abd0a41205d82f403531a46b0cdb0b6
stanley0212 <trip7.net@gmail.com>
2024-05-29 22:08:50 +1000

    modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,10 +28,10 @@ const HomePage = async () => {
           <CategoryList />
         </Suspense>
       </div>
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+      {/* <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
         <NewProductList />
-      </div>
+      </div> */}
     </div>
   )
 }
